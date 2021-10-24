fdbd2c131ecb5fb055f8bb9ab352665cfa8a8341
Krutika Pathak <krutikapathak084@gmail.com>
2021-10-23 19:27:07 -0700

    Memory design initial commit

---
+++ examples/org/cloudbus/cloudsim/examples/power/planetlab/LrMu.java
@@ -0,0 +1,71 @@
+package org.cloudbus.cloudsim.examples.power.planetlab;
+
+import java.io.IOException;
+
+/**
+ * A simulation of a heterogeneous power aware data center that applies the Local Regression (LR) VM
+ * allocation policy and Minimum Utilization (MU) VM selection policy.
+ * 
+ * This example uses a real PlanetLab workload: 20110303.
+ * 
+ * The remaining configuration parameters are in the Constants and PlanetLabConstants classes.
+ * 
+ * If you are using any algorithms, policies or workload included in the power package please cite
+ * the following paper:
+ * 
+ * Anton Beloglazov, and Rajkumar Buyya, "Optimal Online Deterministic Algorithms and Adaptive
+ * Heuristics for Energy and Performance Efficient Dynamic Consolidation of Virtual Machines in
+ * Cloud Data Centers", Concurrency and Computation: Practice and Experience (CCPE), Volume 24,
+ * Issue 13, Pages: 1397-1420, John Wiley & Sons, Ltd, New York, USA, 2012
+ * 
+ * @author Anton Beloglazov
+ * @since Jan 5, 2012
+ */
+public class LrMu {
+
+	/**
+	 * The main method.
+	 * 
+	 * @param args the arguments
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	public static void main(String[] args) throws IOException {
+		boolean enableOutput = true;
+		boolean outputToFile = true;
+		String outputFolder = "output";
+//		String workload = "1"; // PlanetLab workload
+		String vmAllocationPolicy = "lr"; // Local Regression (LR) VM allocation policy
+		String vmSelectionPolicy = "mu"; // Minimum Utilization (MU) VM selection policy
+		String parameter = "1.2"; // the safety parameter of the LR policy
+		String[] planetlabArr = {"20110303", "20110306", "20110309", "20110322", "20110325", "20110403", "20110409", 
+				"20110411", "20110412", "20110420"}; // 10 days PlanetLab data folders
+		String[] bitbrainArr = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}; // 10 days BitBrain data folders
+		for(String filename : planetlabArr) {
+			String inputFolder = LrMu.class.getClassLoader().getResource("workload/PlanetLab").getPath();
+			String workload = filename; 
+			new PlanetLabRunner(
+					enableOutput,
+					outputToFile,
+					inputFolder,
+					outputFolder,
+					workload,
+					vmAllocationPolicy,
+					vmSelectionPolicy,
+					parameter);
+		}
+		
+		for(String filename : bitbrainArr) {
+			String inputFolder = LrMu.class.getClassLoader().getResource("workload/BitBrain").getPath();
+			String workload = filename; 
+			new PlanetLabRunner(
+					enableOutput,
+					outputToFile,
+					inputFolder,
+					outputFolder,
+					workload,
+					vmAllocationPolicy,
+					vmSelectionPolicy,
+					parameter);
+		}
+	}
+}
